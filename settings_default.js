a836c487dac1fbf2be7f3be2d7a8e7a13efef7ef
Jis van Overschot <99040091@mydavinci.nl>
2017-09-30 23:13:24 +0200

    Corrected spelling mistake

--- settings_default.js
+++ settings_default.js
@@ -2,7 +2,7 @@ let settingsObj = {
 	//between 1 and 50
 	maxInPlaylist: '50',
 	//high, medium, off
-	imgSaveSearch: 'off',
+	imgSafeSearch: 'off',
 	//stansrd Overwatch, can be changed into anything, or leave it empty
 	gamePlaying: 'Overwatch'
 }
